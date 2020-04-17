47cf69626997262533ecaeb87c88a236ee23bf67
Chioma Joshua <chiomajoshua@gmail.com>
2020-04-17 04:49:22 +0100

    Added APIResponse<T> Model

---
+++ Necessitudo.APIModels/APIResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Necessitudo.APIModels
+{
+    public class ApiResponse
+    {
+        public int ResponseCode { get; set; }
+        public string RespMessage { get; set; }
+        public int RecordCount { get; set; }
+    }
+
+    public class ApiResponse<T> : ApiResponse
+    {
+        public T ResponseObject { get; set; }
+    }
+   
+    public class ApiResponseList<T> where T : class
+    {
+        public int ResponseCode { get; set; }
+        public string RespMessage { get; set; }
+        public List<T> ResponseObject { get; set; }
+        public int RecordCount { get; set; }
+    }
+}
